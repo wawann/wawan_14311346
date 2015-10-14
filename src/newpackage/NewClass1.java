a3740df6bd8719b575950a96a3b353c94077c5e5
wawann <yeatull@gmail.com>
2015-10-14 10:16:59 +0700

    versi 2.6.1

---
+++ src/newpackage/NewClass1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Wawan
+ */
+public class NewClass1 {
+    public int Hasil;
+    double pembagian;
+    
+    int penjumlahan(int a,int b){
+        Hasil=a+b;
+        return Hasil;
+    }
+    int pengurangan(int a,int b){
+        Hasil=a-b;
+        return Hasil;
+    }
+    int perkalian(int a,int b){
+        Hasil=a*b;
+        return Hasil;
+    }
+    double pembagian(double j,double k){
+        double jumlah ;
+    return jumlah=j/k;
+    }
+}
